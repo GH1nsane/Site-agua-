72f26224a030ba44ad771ebd7656996b5e310c1e
Hikari <127319022+GH1nsane@users.noreply.github.com>
2024-10-08 07:46:26 -0300

    dsad

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 async function visualizarDados(){
     const res = await fetch(url)
     const dados = await res.json()
-    regiao = 0
+    regiao = Brasil
      resultado_dados = (dados[0]['resultados'][0]["series"][regiao]['serie'])
     resultado_regiao = (dados[0]['resultados'][0]["series"][regiao]["localidade"]['nome'])
     console.log(resultado_dados, resultado_regiao)
