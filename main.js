2f61fc3b8cd2acf8b52ddeff7e7b5f839b36d9d9
Hikari <127319022+GH1nsane@users.noreply.github.com>
2024-10-23 09:09:49 -0300

    vfhf

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
+const url = "https://servicodados.ibge.gov.br/api/v3/agregados/4250/periodos/2019/variaveis/4226?localidades=N1[all]|N3[41,42,43]&classificacao=2[110095,110096]|1[1]"
+
 async function visualizarDados(){
     const res = await fetch(url)
     const dados = await res.json()
-    regiao = Brasil
-     resultado_dados = (dados[0]['resultados'][0]["series"][regiao]['serie'])
+    regiao = 0
+    resultado_dados = (dados[0]['resultados'][0]["series"][regiao]['serie'])
     resultado_regiao = (dados[0]['resultados'][0]["series"][regiao]["localidade"]['nome'])
-    console.log(resultado_dados, resultado_regiao)
-  }
-  visualizarDados()
+    
+  
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos--container__texto')
+    paragrafo.innerHTML ="Aqui será colocado os dados obtidos no site do IBGE"
+ 
+    const container = document.getElementById('graficos--container')
+
+    container.appendChild(paragrafo)
+
+}   
+
+
+visualizarDados()
   
